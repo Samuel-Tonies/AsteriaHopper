b3cf96435748c0f7373cd8f45a5556f3c894d9ba
Samuel-Tonies <sftonies@cpp.edu>
2026-04-14 10:33:10 -0700

    working on sim

--- SpringModels.m
+++ SpringModels.m
@@ -1,32 +1,37 @@
 
 % Fuel slosh parameters
 m_s = 355.9/2; % 50% fill
-k_s = 300;
-c_s = 20;
+k_s = 500;
+c_s = 2;
 
 % Floor parameters
-k_T = .2;
-c_T = .4;
+k_T = 50000;
+c_T = 230000;
 mu_floor = .2;
 % Define floor vector
 floor_angle = 6; %deg
-n_floor = [cosd(90+floor_angle), 0, sind(90+floor_angle)];
-
+% n_floor = [cosd(90+floor_angle), 0, sind(90+floor_angle)];
+n_floor = [0 0 1];
 % Leg Model
 angle_legs = 54; %degrees
 body_length = 1.61544;
 leg_length = 1.87198;
 
-r_l1 = [cosd(angle_legs); 0; sind(leg_length)];
-r_l2 = [-cosd(angle_legs); 0; sind(leg_length)];
-r_l3 = [0; cosd(angle_legs); sind(leg_length)];
-r_l4 = [0; -cosd(angle_legs); sind(leg_length)];
+r_l1 = [cosd(angle_legs); 0; sind(angle_legs)];
+r_l1 = r_l1/norm(r_l1);
+r_l2 = [-cosd(angle_legs); 0; sind(angle_legs)];
+r_l2 = r_l2/norm(r_l2);
+r_l3 = [0; cosd(angle_legs); sind(angle_legs)];
+r_l3 = r_l3/norm(r_l3);
+r_l4 = [0; -cosd(angle_legs); sind(angle_legs)];
+r_l4 = r_l4/norm(r_l4);
 
-p_l1 = [-1; 0; .5];
-p_l2 = [1; 0; .5];
-p_l3 = [0; -1; .5];
-p_l4 = [0; 1; .5]; %UPDATE ALL
+p_l1 = [-1; 0; -.5];
+p_l2 = [1; 0; -.5];
+p_l3 = [0; -1; -.5];
+p_l4 = [0; 1; -.5]; %UPDATE ALL
 
-k_l = .5;
-c_l = .4;
+k_l = 1;
+c_l = 1;
 m_leg = 10;
+leg_length = 1;
--- plot_junk.m
+++ plot_junk.m
@@ -4,9 +4,20 @@ X_i = data.simout.X_i;
 z = X_i.data(:,3);
 x = X_i.data(:,1);
 t = X_i.Time;
+g
+euler_a_rad = data.simout.euler_a_rad.Data;
+pitch = rad2deg(euler_a_rad(:,2));
 
 figure
-plot(t,x)
-hold on
 plot(t,z)
-title("Vehicle States")
+title("Vehicle Altitude")
+xlabel("Time [s]")
+ylabel("Altitude [m]")
+grid on
+
+figure
+plot(t,pitch)
+title("Vehicle Pitch")
+xlabel("Time [s]")
+ylabel("Pitch [deg]")
+grid on
